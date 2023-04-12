d9e11fef9b37f7843b9e8e7047a0176ea3510e80
ogunyanik <ogunyanik@gmail.com>
2023-04-12 16:33:27 +0300

    main.go updated

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World  v2👋!")
+		return c.SendString("Hello, World  v3👋!")
 	})
 
 	app.Listen(":80")
