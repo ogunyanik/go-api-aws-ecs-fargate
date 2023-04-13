e764cd5512dc4241a69e2588ae41bcd820e9fa49
ogunyanik <ogunyanik@gmail.com>
2023-04-13 16:24:00 +0300

    main.go updated
    Signed-off-by: ogunyanik <ogunyanik@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-
+	fmt.Println("Started")
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World  v4👋!")
 	})
 
+	app.Get("/late", func(c *fiber.Ctx) error {
+		time.Sleep(2 * time.Second)
+		return c.SendString("Hello late World  v4👋!")
+	})
 	app.Listen(":80")
 }
